3be5463b5c8837593379c17cd374f8814d2a7939
aliprofessionalme-ux <ali.professional.me@gmail.com>
2026-06-11 01:23:47 +0500

    Harden custom mobile Expo entry

--- artifacts/mobile/index.js
+++ artifacts/mobile/index.js
@@ -2,9 +2,9 @@ import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
 import React from "react";
 
-function App() {
+export function App() {
   const context = require.context("./app");
-  return <ExpoRoot context={context} />;
+  return React.createElement(ExpoRoot, { context });
 }
 
 registerRootComponent(App);
